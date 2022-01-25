6ecff17abead18a026a2bd41aa4e193276192c24
Julian-H-Martinez <julian.h.martinez10@gmail.com>
2022-01-25 12:13:18 -0600

    walkthrough with David; refactored some values to be uniformed for future exercises

--- albums_migration.sql
+++ albums_migration.sql
@@ -2,11 +2,11 @@ USE codeup_test_db;
 DROP TABLE IF EXISTS albums;
 CREATE TABLE albums(
    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-   artist VARCHAR(100),
-   name VARCHAR(100),
-   release_date INT,
-   sales FLOAT,
-   genre VARCHAR (50),
+   artist VARCHAR(200) NOT NULL,
+   name VARCHAR(200) NOT NULL,
+   release_date INT UNSIGNED,
+   sales FLOAT UNSIGNED,
+   genre VARCHAR (100),
    PRIMARY KEY(id)
 );
 SHOW TABLES;
