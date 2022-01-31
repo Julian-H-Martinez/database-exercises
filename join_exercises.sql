e2537f64ddf728458695311121cd29330146ae08
Julian-H-Martinez <julian.h.martinez10@gmail.com>
2022-01-31 13:49:43 -0600

    completed problem 'Find the name of all departments currently managed by women.'

--- join_exercises.sql
+++ join_exercises.sql
@@ -33,3 +33,26 @@ FROM employees AS e
         ON dept.dept_no = dm.dept_no
 WHERE dm.to_date = '9999-01-01'
 ORDER BY `Department Name`;
+
+SELECT dept.dept_name AS 'Department Name',
+       CONCAT(e.first_name, ' ', e.last_name) AS 'Department Manager'
+FROM employees AS e
+    JOIN dept_manager AS dm
+        ON e.emp_no = dm.emp_no
+    JOIN departments AS dept
+        ON dept.dept_no = dm.dept_no
+    JOIN employees AS emp
+        ON emp.emp_no = dm.emp_no
+WHERE emp.gender = 'f'
+    AND dm.to_date = '9999-01-01'
+ORDER BY 'Department Name';
+# Find the name of all departments currently managed by women.
+#
+# +------------------+--------------------+
+# | Department Name  | Department Manager |
+# +------------------+--------------------+
+# | Development      | Leon DasSarma      |
+# | Finance          | Isamu Legleitner   |
+# | Human Resources  | Karsten Sigstam    |
+# | Research         | Hilary Kambil      |
+# +------------------+--------------------+
