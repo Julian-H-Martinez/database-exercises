5021d02db62bdfa6a13cd56648be61bc9c3e8bd4
Julian-H-Martinez <julian.h.martinez10@gmail.com>
2022-02-01 09:58:54 -0600

    completed problem: 'Find all the titles held by all employees with the first name Aamod.'

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -1,32 +1,34 @@
 USE employees;
 # Find all the employees with the same hire date as employee 101010 using a subquery.
-# WHAT WE WANT -> WHAT INFO ARE WE PULLING FROM TABLES -> first_name, last_name, hire_date
+# 69 Rows Expected
+
+    # WHAT WE WANT -> WHAT INFO ARE WE PULLING FROM TABLES -> first_name, last_name, hire_date
 SELECT first_name, last_name, hire_date
-# WHAT TABLE ARE WE LOOKING TO GET THEM FROM -> employees
+    # WHAT TABLE ARE WE LOOKING TO GET THEM FROM -> employees
 FROM employees
-# FILTER RESULTS TO GET CERTAIN EMPLOYEES BY -> hire_date
+    # FILTER RESULTS TO GET CERTAIN EMPLOYEES BY -> hire_date
 WHERE hire_date IN (
-# WHAT WE WANT -> hire_date
+        # WHAT WE WANT -> hire_date
     SELECT hire_date
-# WHAT TABLE ARE WE LOOKING TO GET THEM FROM -> employees
+        # WHAT TABLE ARE WE LOOKING TO GET THEM FROM -> employees
     FROM employees
-# FILTER RESULTS BY A CERTAIN EMPLOYEE -> emp_no
+        # FILTER RESULTS BY A CERTAIN EMPLOYEE -> emp_no
     WHERE emp_no = 101010
     );
-# SELECT first_name, last_name, birth_date
-# FROM employees
-# WHERE emp_no IN (
-#     SELECT emp_no
-#     FROM dept_manager
-# )
-# LIMIT 10;
-#
-# 69 Rows
 #
 # Find all the titles held by all employees with the first name Aamod.
-#
 # 314 total titles, 6 unique titles
-#
+    # titles table
+    # employees table
+
+SELECT title
+FROM titles
+WHERE emp_no IN(
+    SELECT emp_no
+    FROM employees
+    WHERE first_name = 'Aamod'
+    );
+
 # Find all the current department managers that are female.
 #
 #
